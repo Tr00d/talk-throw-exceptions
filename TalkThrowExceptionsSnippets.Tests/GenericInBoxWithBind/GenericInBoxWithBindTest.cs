4678e0a50987ce572d38b28fb1a443bdcaf3683a
Guillaume Faas <guillaume.faas@live.fr>
2023-11-07 14:22:25 +0100

    Add generic box with Bind example

---
+++ TalkThrowExceptionsSnippets.Tests/GenericInBoxWithBind/GenericInBoxWithBindTest.cs
@@ -0,0 +1,68 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace TalkThrowExceptionsSnippets.Tests.GenericInBoxWithBind;
+
+public class GenericInBoxWithBindTest
+{
+	[Fact]
+	public void Bind_ShouldExecuteFunction_GivenValueIsSome() =>
+		SchrodingerBox<int>.Some(0)
+			.Bind(Increment)
+			.Bind(Increment)
+			.Bind(Increment)
+			.OpenBox()
+			.Should()
+			.Be(3);
+
+	[Fact]
+	public void Bind_ShouldReturnEmptyBox_GivenValueExceedsThree() =>
+		SchrodingerBox<int>.Some(0)
+			.Bind(Increment)
+			.Bind(Increment)
+			.Bind(Increment)
+			.Bind(Increment)
+			.OpenBox()
+			.Should()
+			.Be(null);
+
+	private static SchrodingerBox<int> Increment(int value) =>
+		value < 3 ? SchrodingerBox<int>.Some(value + 1) : SchrodingerBox<int>.None();
+
+	internal class SchrodingerBox<T> where T : struct
+	{
+		private bool IsSome { get; }
+		private readonly T value;
+
+		// Constructor for Some
+		private SchrodingerBox(T value)
+		{
+			this.value = value;
+			this.IsSome = true;
+		}
+
+		// Constructor for None
+		private SchrodingerBox() => this.IsSome = false;
+
+		// Applies the function on the value IF our box contains a value
+		public SchrodingerBox<TResult> Bind<TResult>(Func<T, SchrodingerBox<TResult>> bind) where TResult : struct =>
+			this.IsSome
+				? bind(this.value)
+				: SchrodingerBox<TResult>.None();
+
+		// Applies the function on the value IF our box contains a value
+		public SchrodingerBox<TResult> Map<TResult>(Func<T, TResult> map) where TResult : struct =>
+			this.IsSome
+				? Some(map(this.value))
+				: SchrodingerBox<TResult>.None();
+
+		// Creates a box without a value
+		public static SchrodingerBox<T> None() => new();
+
+		public T? OpenBox() => this.IsSome ? this.value : null;
+
+		// Creates a box with a value
+		public static SchrodingerBox<TResult> Some<TResult>(TResult value) where TResult : struct => new(value);
+	}
+}
