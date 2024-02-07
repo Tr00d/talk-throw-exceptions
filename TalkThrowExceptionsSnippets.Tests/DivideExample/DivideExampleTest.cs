e2cdf3f0392689e019b69e1899607a3cfc126d59
Tr00d <guillaume.faas@live.fr>
2024-02-07 12:00:32 +0100

    Update divide example

--- TalkThrowExceptionsSnippets.Tests/DivideExample/DivideExampleTest.cs
+++ TalkThrowExceptionsSnippets.Tests/DivideExample/DivideExampleTest.cs
@@ -1,5 +1,6 @@
 using System;
 using LanguageExt;
+using TalkThrowExceptionsSnippets.Tests.ExceptionExample;
 
 namespace TalkThrowExceptionsSnippets.Tests.DivideExample;
 
@@ -13,7 +14,7 @@ public class DivideExampleTest
     public void Main()
     {
         double a = 98, b = 0;
-        var text = SafeDivision(a, b).Match(success => $"{a} divided by {b} = {success}", _ => _);
+        var text = SafeDivision(a, b).IfRight(success => $"{a} divided by {b} = {success}");
         Console.WriteLine(text);
     }
 
@@ -21,4 +22,25 @@ public class DivideExampleTest
         y == 0
             ? Either<string, double>.Left("Attempted divide by zero.")
             : Either<string, double>.Right(x / y);
+}
+
+public class Implementation1 : ICanDivide
+{
+    public decimal Divide(decimal x, decimal y) => y == 0 ? 0 : x / y;
+}
+
+public class Implementation2 : ICanDivide
+{
+    private const int DefaultMagicValue = 123;
+    public decimal Divide(decimal x, decimal y) => y == 0 ? DefaultMagicValue : x / y;
+}
+
+public class Implementation3 : ICanDivide
+{
+    public decimal Divide(decimal x, decimal y) => y == 0 ? throw new DivideByZeroException() : x / y;
+}
+
+public class Implementation4 : ICanDivide
+{
+    public decimal Divide(decimal x, decimal y) => y == 0 ? throw new Exception() : x / y;
 }
