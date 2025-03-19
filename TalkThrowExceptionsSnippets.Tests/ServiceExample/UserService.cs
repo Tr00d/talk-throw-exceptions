3b19c23069c4f4c7c2d94ca810a3631a30fe593c
Tr00d <guillaume.faas@live.fr>
2025-03-19 11:10:08 +0100

    feat: add usage for user service

--- TalkThrowExceptionsSnippets.Tests/ServiceExample/UserService.cs
+++ TalkThrowExceptionsSnippets.Tests/ServiceExample/UserService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using LanguageExt;
+using Xunit;
 
 namespace TalkThrowExceptionsSnippets.Tests.ServiceExample;
 
@@ -17,6 +18,13 @@ public class UserService
         }
 
         return this.dataStore.FirstOrDefault(user => user.Email == email);
+    }
+
+    [Fact]
+    public void Usage()
+    {
+        var service = new UserService();
+        var user = service.FindUser("guillaume.faas@vonage.com");
     }
 }
 
@@ -34,22 +42,62 @@ public class UserServiceMonad
         return this.dataStore.FirstOrDefault(user => user.Email == email)
                ?? Either<Error, User>.Left(new MissingUser($"User '{email}' is missing"));
     }
-    
+
     public Either<Error, User> FindUserRefactored(string email) => ValidateEmail(email).Bind(this.LookupUser);
+
+    [Fact]
+    public void Usage()
+    {
+        var service = new UserServiceMonad();
+        var result = service.FindUser("guillaume.faas@vonage.com");
+        result.Match(this.UserFound, this.OnError);
+    }
+
+    [Fact]
+    public void UsageWithPatternMatching()
+    {
+        var service = new UserServiceMonad();
+        var result = service.FindUser("guillaume.faas@vonage.com");
+        result.Match(this.UserFound, error => error switch
+        {
+            ValidationFailed validationFailed => this.OnValidationFailed(validationFailed),
+            MissingUser missingUser => this.OnMissingUser(missingUser),
+            _ => this.OnError(error),
+        });
+    }
+
+    private Either<Error, User> LookupUser(string email) =>
+        this.dataStore.FirstOrDefault(user => user.Email == email)
+        ?? Either<Error, User>.Left(new MissingUser($"User '{email}' is missing"));
+
+    private Unit OnError(Error error)
+    {
+        return Unit.Default;
+    }
+
+    private Unit OnMissingUser(MissingUser missing)
+    {
+        return Unit.Default;
+    }
+
+    private Unit OnValidationFailed(ValidationFailed failed)
+    {
+        return Unit.Default;
+    }
+
+    private Unit UserFound(User user)
+    {
+        return Unit.Default;
+    }
 
     private static Either<Error, string> ValidateEmail(string email) =>
         string.IsNullOrWhiteSpace(email)
             ? Either<Error, string>.Left(new ValidationFailed("Email cannot be null or empty"))
             : Either<Error, string>.Right(email);
 
-    private Either<Error, User> LookupUser(string email) =>
-        this.dataStore.FirstOrDefault(user => user.Email == email)
-        ?? Either<Error, User>.Left(new MissingUser($"User '{email}' is missing"));
+    public abstract record Error(string Reason);
+    public record ValidationFailed(string Reason) : Error(Reason);
+    public record MissingUser(string Reason) : Error(Reason);
 }
-
-public abstract record Error(string Reason);
-public record ValidationFailed(string Reason) : Error(Reason);
-public record MissingUser(string Reason) : Error(Reason);
-
 
 public record User(string Email);
